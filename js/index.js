4d59d5f75ea80a2720d20e1e7b2f9cd22debbe63
Sean Howe <seanmarchowe@gmail.com>
2023-02-10 11:02:08 +0000

    added singles display and overlay

--- js/index.js
+++ js/index.js
@@ -7,14 +7,19 @@ const options = {
 };
 
 const searchResultPage = document.getElementById("search-results");
+const artistPage = document.getElementById("artist-results");
+const artistInfo = document.getElementById("artistInfo");
+const singlesDisplay = document.getElementById("singles-display");
+const albumsDisplay = document.getElementById("albums-display");
 const searchResultsDiv = document.createElement("div");
 searchResultsDiv.setAttribute("class", "resultsContainer");
+searchResultPage.append(searchResultsDiv);
 
 // Fetches artists information from user input field
 // NOTE: We need to use encodeURI with the input value
 async function getSearchData() {
   let res = await fetch(
-    `https://spotify81.p.rapidapi.com/search?q=robbie%20williams&type=artists&offset=0&limit=10&numberOfTopResults=5`,
+    `https://spotify81.p.rapidapi.com/search?q=robbie%20williams&type=artists&offset=0&limit=8&numberOfTopResults=5`,
     options
   );
   let data = await res.json();
@@ -23,7 +28,6 @@ async function getSearchData() {
   for (let i = 0; i < artistInfo.length; i++) {
     displaySearchResults(artistInfo[i]);
   }
-  searchResultPage.append(searchResultsDiv);
 }
 getSearchData();
 // Fetches artist data once artist ID has been retrieved from getSearchData function
@@ -42,7 +46,8 @@ async function getArtistSingles(id) {
     options
   );
   let data = await res.json();
-  console.log(data);
+  let singles = data.data.artist.discography.singles;
+  return singles;
 }
 
 // Fetch artist albums using artist id retrieved from getSearchData function
@@ -52,7 +57,8 @@ async function getArtistAlbums(id) {
     options
   );
   let data = await res.json();
-  console.log(data);
+  let albums = data.data.artist.discography.albums;
+  return albums;
 }
 
 function searchResultsLoop(results) {
@@ -60,22 +66,51 @@ function searchResultsLoop(results) {
     console.log(results[i].data);
   }
 }
-
+// Displays the search results on a search results page
 function displaySearchResults(artistInfo) {
   // Create elements to display search results data
-
   const resultCard = document.createElement("div");
   resultCard.setAttribute("class", "card");
-
   // Search results required data
   const artistName = artistInfo.data.profile.name;
   const artistImage = artistInfo.data.visuals.avatarImage.sources[0].url;
   const artistID = artistInfo.data.uri.split(":").pop();
+  resultCard.setAttribute("data-id", artistID);
   resultCard.innerHTML = `
-    <a href="${artistID}">
       <h1>${artistName}</h1>
       <img src="${artistImage}"></img>
-    </a>
   `;
   searchResultsDiv.append(resultCard);
+  resultCard.addEventListener("click", () => {
+    let spotifyId = resultCard.dataset.id;
+    ArtistDetail(spotifyId);
+  });
+}
+
+function ArtistDetail(id) {
+  getArtistSingles(id).then(artistSinglesDisplay);
+  // getArtistAlbums(id).then(artistAlbumsDisplay);
+}
+
+// function artistAlbumsDisplay(data) {
+//   console.log(data.items);
+// }
+function artistSinglesDisplay(data) {
+  let singles = data.items;
+  singlesDisplay.innerHTML = singles
+    .map(
+      (item) =>
+        `
+      <div class="single-item" id="${item.releases.items[0].id}">
+        <div class="mr-2">
+          <img src="${item.releases.items[0].coverArt.sources[1].url}"></img>
+        </div>
+          <h4 class="mr-2">${item.releases.items[0].name}</h4>
+          <a target="_blank" href="${item.releases.items[0].sharingInfo.shareUrl}">
+            <i class="fa-solid fa-play"></i>
+          </a>
+      </div>
+      `
+    )
+    .join("");
 }
